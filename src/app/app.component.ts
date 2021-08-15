605f0414e84c5a7f53be4ca8e2461e72e19b615d
Raccoon2508 <ridge2006@mail.ru>
2021-08-15 23:40:56 +0300

    Update app structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  styles: [
+    `h3 {
+      color: blue;
+    }`
+  ]
 })
 export class AppComponent {
   title = 'new-app';
+  name = 'Nikita';
 }
